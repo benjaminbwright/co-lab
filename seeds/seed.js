2cf0dea8e32937ddfce75c4aa2a5f074c69c5122
Benjamin Wright <bwright@trilogyed.com>
2023-03-22 10:22:51 -0500

    adds user seeds

---
+++ seeds/seed.js
@@ -0,0 +1,23 @@
+// sequelize connection
+const sequelize = require('../config/connection');
+// eslint-disable-next-line
+const { User } = require('../models');
+
+// data
+const userData = [
+  {
+    username: 'bilbobaggins',
+    email: 'bilbo@bagend.com',
+    password: 'GollumCantCatchMe#1!!!',
+  },
+];
+
+// seed function
+const seedDB = async () => {
+  await sequelize.sync({ force: true });
+  await User.bulkCreate(userData);
+  // seed some stuff.
+  process.exit(0);
+};
+
+seedDB();
