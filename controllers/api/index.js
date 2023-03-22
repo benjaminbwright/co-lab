01431b1145b0f2d7fd4fc3e7218fc5164a9fd87a
Benjamin Wright <bwright@trilogyed.com>
2023-03-22 15:17:00 -0500

    destructures user from session object

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -9,48 +9,52 @@ apiRouter.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
     if (!username || !password) {
-      return res.status(400).json({ message: 'username or password missing'});
+      return res.status(400).json({ message: 'username or password missing' });
     }
 
     const userData = await User.findOne({
       where: {
-        username
-      }
+        username,
+      },
     });
 
-    const user = userData.get({plain: true});
+    const user = userData.get({ plain: true });
 
     if (password !== user.password) {
-      return res.status(400).json({ message: 'password does not match' })
+      return res.status(400).json({ message: 'password does not match' });
     }
 
     req.session.save(() => {
       req.session.user = {
         id: user.id,
         username: user.username,
-        email: user.email
-      }
-      
-      return res.json({ message: `${username} successfully logged in and session user is ${req.session.user.username}`});
-    })
-    
+        email: user.email,
+      };
+
+      return res.json({
+        message: `${username} successfully logged in and session user is ${req.session.user.username}`,
+      });
+    });
+
     return 0;
-  } catch(err) {
-    return res.status(500).json({ err })
+  } catch (err) {
+    return res.status(500).json({ err });
   }
-})
+});
 
 apiRouter.get('/logout', (req, res) => {
-  
-  const { session: user } = req;
+  const {
+    session: { user },
+  } = req;
   if (!user) {
-    return res.status(400).json({ message: 'user not logged in'});
+    return res.status(400).json({ message: 'user not logged in' });
   }
+  console.log(user);
 
   const { username } = user;
 
   req.session.destroy();
-  return res.json({ message: `user ${username} successfully logged out`});
-})
+  return res.json({ message: `user ${username} successfully logged out` });
+});
 
 module.exports = apiRouter;
