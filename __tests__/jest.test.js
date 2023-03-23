9b198aa03f7dc1f6d7e9c1481f4a454ccd661729
max <max@ubuntu.ubuntu.local>
2023-03-23 14:58:18 -0400

    adds user model tests

--- __tests__/jest.test.js
+++ __tests__/jest.test.js
@@ -1,4 +1,6 @@
 const sequelize = require('../config/connection');
+const { User } = require('../models');
+
 
 beforeAll(async () => {
   await sequelize.sync();
@@ -12,6 +14,38 @@ describe('Jest is running', () => {
   describe('Sample Test', () => {
     it('should pass', () => {
       expect(5).toEqual(5);
+    });
+  });
+});
+
+describe('Sequelize User Model Tests', () => {
+
+  describe('Creates User Model Instance', () => {
+
+    it('should create a new User Model Instance', async () => {
+      const userData = await User.create({
+        username: 'testuser',
+        email: 'testuser@teststuff.net',
+        password: 'testpassword'
+      });
+      const user = userData.get({plain: true});
+      expect(user).toBeDefined();
+    });
+
+    it('should create a new User Model Instance with the correct properties and values', async () => {
+      const userData = await User.create({
+        username: 'testuser2',
+        email: 'testuser2@teststuff.net',
+        password: 'testpassword'
+      });
+      const user = userData.get({plain: true});
+      
+      expect(user).toEqual({
+        id: user.id,
+        username: 'testuser2',
+        email: 'testuser2@teststuff.net',
+        password: user.password
+      });
     });
   });
 });
